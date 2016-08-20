718862e96b80bbaa89339e24d0d65528ceb62a88
Roman S <romeo97934@gmail.com>
2016-08-20 23:16:39 +0000

    TIL bugs fixed & Gradle dependencies updated & New register validations added

--- app/src/main/java/com/android/multilevelmarketing/ui/activity/RegisterActivity.java
+++ app/src/main/java/com/android/multilevelmarketing/ui/activity/RegisterActivity.java
@@ -5,8 +5,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.Snackbar;
+import android.support.design.widget.TextInputLayout;
 import android.util.Log;
-import android.widget.EditText;
 import android.widget.RelativeLayout;
 
 import com.android.multilevelmarketing.R;
@@ -16,6 +16,8 @@ import com.android.multilevelmarketing.data.sharedprefs.SharedPrefUtils;
 import com.android.multilevelmarketing.ui.dialog.LoadingDialog;
 import com.android.multilevelmarketing.util.RetrofitUtils;
 
+import butterknife.BindInt;
+import butterknife.BindString;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
@@ -29,29 +31,52 @@ public class RegisterActivity extends BaseAnimActivity {
     @BindView(R.id.root_view)
     public RelativeLayout rootView;
 
-    @BindView(R.id.et_refer)
-    public EditText mainRefer;
+    @BindView(R.id.til_refer)
+    public TextInputLayout TILMainRefer;
 
-    @BindView(R.id.et_main_name)
-    public EditText mainName;
+    @BindView(R.id.til_main_name)
+    public TextInputLayout TILMainName;
 
-    @BindView(R.id.et_main_surname)
-    public EditText mainSurname;
+    @BindView(R.id.til_main_surname)
+    public TextInputLayout TILMainSurname;
 
-    @BindView(R.id.et_main_email)
-    public EditText mainEmail;
+    @BindView(R.id.til_main_email)
+    public TextInputLayout TILMainEmail;
 
-    @BindView(R.id.et_main_phone)
-    public EditText mainPhone;
+    @BindView(R.id.til_main_phone)
+    public TextInputLayout TILMainPhone;
 
-    @BindView(R.id.et_username)
-    public EditText mainUsername;
+    @BindView(R.id.til_username)
+    public TextInputLayout TILMainUsername;
 
-    @BindView(R.id.et_password)
-    public EditText mainPassword;
+    @BindView(R.id.til_password)
+    public TextInputLayout TILMainPassword;
 
-    @BindView(R.id.et_password_repeat)
-    public EditText mainPasswordRepeat;
+    @BindView(R.id.til_password_repeat)
+    public TextInputLayout TILMainPasswordRepeat;
+
+    /**
+     * ERROR STRINGS
+     **/
+    @BindString(R.string.register_error_passwords_not_matching)
+    public String ERROR_PASSWORDS_DO_NOT_MATCH;
+
+    @BindString(R.string.register_error_empty_field)
+    public String ERROR_EMPTY_FIELD;
+
+    @BindString(R.string.register_error_contains_spaces)
+    public String ERROR_FIELD_CONTAINS_SPACES;
+    /**
+     * INTEGERS
+     **/
+    @BindInt(R.integer.phone_max_length)
+    public int MAX_LENGTH_PHONE;
+
+    @BindInt(R.integer.username_max_length)
+    public int MAX_LENGTH_USERNAME;
+
+    @BindInt(R.integer.password_max_length)
+    public int MAX_LENGTH_PASSWORD;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -63,16 +88,31 @@ public class RegisterActivity extends BaseAnimActivity {
     @OnClick(R.id.btn_register)
     public void register() {
         final ProgressDialog loadingDialog = LoadingDialog.create(this);
-        loadingDialog.show();
 
-        String refer = mainRefer.getText().toString();
-        String name = mainName.getText().toString();
-        String surname = mainSurname.getText().toString();
-        String email = mainEmail.getText().toString();
-        String phone = mainPhone.getText().toString();
-        String username = mainUsername.getText().toString();
-        String password = mainPassword.getText().toString();
-        String passwordRepeat = mainPasswordRepeat.getText().toString();
+        String refer = TILMainRefer.getEditText().getText().toString();
+        String name = TILMainName.getEditText().getText().toString();
+        String surname = TILMainSurname.getEditText().getText().toString();
+        String email = TILMainEmail.getEditText().getText().toString();
+        String phone = TILMainPhone.getEditText().getText().toString();
+        String username = TILMainUsername.getEditText().getText().toString();
+        String password = TILMainPassword.getEditText().getText().toString();
+        String passwordRepeat = TILMainPasswordRepeat.getEditText().getText().toString();
+
+        if (!checkForEmpty(TILMainRefer, TILMainName, TILMainSurname,
+                TILMainEmail, TILMainPhone, TILMainUsername, TILMainPassword, TILMainPasswordRepeat)) {
+            return;
+        }
+
+        if (!checkForSpaces(TILMainRefer, TILMainName, TILMainSurname,
+                TILMainEmail, TILMainPhone, TILMainUsername, TILMainPassword, TILMainPasswordRepeat)) {
+            return;
+        }
+
+        if (!validateForMatching(password, passwordRepeat)) {
+            return;
+        }
+
+        loadingDialog.show();
 
         RetrofitAPI.getInstance().register(
                 name,
@@ -125,5 +165,53 @@ public class RegisterActivity extends BaseAnimActivity {
                 onError();
             }
         });
+    }
+
+    private boolean checkForEmpty(TextInputLayout... layouts) {
+        boolean validationPassed = true;
+
+        for (TextInputLayout textInputLayout : layouts) {
+            if (textInputLayout.getEditText().getText().toString().trim().equals("")) {
+                validationPassed = false;
+                textInputLayout.setError(ERROR_EMPTY_FIELD);
+            } else {
+                removeErrorFromTIL(textInputLayout);
+            }
+        }
+
+        return validationPassed;
+    }
+
+    private boolean checkForSpaces(TextInputLayout... layouts) {
+        boolean validationPassed = true;
+
+        for (TextInputLayout textInputLayout : layouts) {
+            if (textInputLayout.getEditText().getText().toString().trim().contains(" ")) {
+                validationPassed = false;
+                textInputLayout.setError(ERROR_FIELD_CONTAINS_SPACES);
+            } else {
+                textInputLayout.setError("");
+            }
+        }
+
+        return validationPassed;
+    }
+
+    private boolean validateForMatching(String str1, String str2) {
+        boolean matched = str1.trim().equals(str2.trim());
+
+        if (!matched) {
+            TILMainPasswordRepeat.setError(ERROR_PASSWORDS_DO_NOT_MATCH);
+        } else {
+            removeErrorFromTIL(TILMainPasswordRepeat);
+        }
+
+        return matched;
+    }
+
+    private void removeErrorFromTIL(TextInputLayout... layouts) {
+        for (TextInputLayout textInputLayout : layouts) {
+            textInputLayout.setError("");
+        }
     }
 }
